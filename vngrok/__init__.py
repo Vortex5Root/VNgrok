e1f3673207d10e875f695570ecbda3f3ee6941ee
vortex <vortex@vortex.lan>
2024-08-29 16:26:20 +0100

    Delete attr fixed!

--- vngrok/__init__.py
+++ vngrok/__init__.py
@@ -89,6 +89,8 @@ class SSH_Reverser_Tunnel:
             kill_port = f"sshpass -p '{self.password}' ssh -o StrictHostKeyChecking=no -p {self.remote_port} "
             kill_port += f"{self.user}@{self.remote_host}"
             kill_port += f'"bash kill.bash {data["remote_port"]}"'
+            self.logger.info(f"Killing port {data['remote_port']} on {self.remote_host}")
+            self.logger.info(kill_port)
             data["listening"].stop_event.set()
             data["stop_event"].set()
             #data["thread"].join()
